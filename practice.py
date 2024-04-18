7f72999cb13e52ac0932917d31ffa7dee0eb264e
shahin <shahinurislama@gmail.com>
2024-04-18 13:31:01 +0530

    added new changes

---
+++ practice.py
@@ -0,0 +1,10 @@
+# Name= "Shahinur"
+# State= "Assam"
+# Country= "India"
+
+# print("my name is :",Name,"I am from :", State, "My Country Name is:", Country)
+Num_1=int(input("Enter the first number:"))
+Num_2=int(input("Enter the second number:"))
+sum=Num_1+Num_2
+print("The sum of the two number:",sum)
+print(type(Num_1))
